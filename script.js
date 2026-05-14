38be9dfac4dc036aa3a4f8d0e7c96b4b9af71421
m1les2 <jamile.mimis@gmail.com>
2026-05-14 16:18:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
